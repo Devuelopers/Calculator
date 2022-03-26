7d1af4a4b28def170a0c6115d5ca1fb10781428b
Leonardo Bertele Tosin <leob.tosin@hotmail.com>
2022-03-25 21:41:14 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  import Body from './components/Body.vue';
-  import Footer from './components/Footer.vue';
+import { defineComponent } from 'vue';
+import Calculator from './components/Calculator.vue';
 
-  export default defineComponent({
-    name: "App",
-    components: { Body, Footer },
-  });
+export default defineComponent({
+  name: "App",
+  components: {
+    Calculator
+  }
+});
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <Body/>
-  <Footer />
+  <Calculator />
 </template>
 
 <style lang="scss">
-  @import './style/App';
+@import "./style/App";
 </style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "Calculator",
+});
+</script>
+
+<template>
+    <form name="frm_calc" acton>
+        <center>
+            <table width="200" cellpadding="3" cellspacing="5"
+                style="color:#FFB90F; background-color:#000000; border:2pt solid #FFB90F">
+                <tr>
+                    <td colspan="5" align="center" bgColor="#FFB90F">
+                        <Font Face="Verdana" Size="4" Color="#000000"><b>Calculadora</b></font>
+                    </td>
+                </tr>
+            </table>
+        </center>
+    </form>
+</template>
+
+<style lang="scss">
+@import "../style/Calculator";
+</style>
